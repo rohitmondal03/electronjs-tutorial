e690b34b2ad5c71c8d342336cd78e712544d0532
Rohit <rohitmondall8000@gmail.com>
2024-04-26 18:28:06 +0530

    initialized and configured thr project

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <span className="text-4xl text-sky-600">Hello  electron</span>
+    </div>
+  )
+}
+
+export default App
